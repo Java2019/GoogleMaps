aa6da9206474a1fddfd6de5e2f9454b888de705f
java2019 <java_2019@mail.ru>
2017-07-12 21:13:06 +0300

    init

--- app/src/main/java/com/nemo/googlemaps/MainActivity.java
+++ app/src/main/java/com/nemo/googlemaps/MainActivity.java
@@ -3,11 +3,40 @@ package com.nemo.googlemaps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final LatLng TutorialsPoint = new LatLng(21 , 57);
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            if (googleMap == null){
+                googleMap = ((MapFragment) getFragmentManager().
+                        findFragmentById(R.id.map)).getMapAsync();
+            }
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP = googleMap
+                    .addMarker(new MarkerOptions()
+                    .position(TutorialsPoint)
+                    .title("TutorialsPoint"));
+        }catch (Exception e){
+
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
